f956ec8df5733815a7f7e96d0301f4f6772f97fa
jbustoro <jbustoro16dw@ikzubirimanteo.com>
2017-11-30 16:38:34 +0100

    Initial commit

---
+++ AJAX/Ejercicio2/index.php
@@ -0,0 +1,11 @@
+<?php
+$fichero = 'peliculas.xml';
+
+if (file_exists($fichero)) {
+    $output = file_get_contents($fichero);
+    header("Content-Type: application/XML");
+    print($output);
+} else {
+    print("No existe el fichero");
+}
+?>
